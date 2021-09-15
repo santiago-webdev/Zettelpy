c21cb58ae89b080a377b74fd6f84207a0e47b7bc
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-09-15 11:46:15 -0300

    Features added, lastOpenedNote, IDs and Title

--- zettelpy/__main__.py
+++ zettelpy/__main__.py
@@ -1,8 +1,20 @@
-import argparse, sys, os, subprocess
+import argparse, sys, os, subprocess, re
+from os.path import exists
 from pathlib import Path
 
 class NoteError(Exception):
-    pass
+    pass # For managing exceptions
+
+def cli() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(prog='zettelpy',
+        description='Personal Knowledge System based on Zettelkasten')
+    parser.add_argument('destination',
+        type=Path,
+        help='Provide path/Title or Luhmann-ID to go to note')
+    parser.add_argument('-v', '--view',
+        action='store_true',
+        help='Open a note by title, ID, or by lastNoteUsed')
+    return parser.parse_args()
 
 def slip_box(directory):
     try:
@@ -16,59 +28,50 @@ def slip_box(directory):
         print('Error: Could not create directory', d, file=stderr)
         exit(1)
 
-def what_is_this(dest: Path):
+def define_path(dest: Path):
     if str(dest) == 'lastOpenedNote':
-        print('Open the lastOpenedNote')
         with open('utils/lastOpenedNote', 'r') as readLastNote:
             temp = readLastNote.read().rstrip('\n')
-            readLastNote.close()
             return temp
     else:
-        return dest
-    # destChar = str(dest)[:1]
-    # print(destChar)
-    # if (destChar == '@'):
-    #     print('This is a tag')
-    #     # and now it should search for a Luhmann-ID inside a note, to open it
-    # else:
-    #     print('This is not a tag')
-
+        if (str(dest)[:1] == '@'):
+            auxVar = subprocess.check_output(['rg', '-w', '-l', dest])
+            auxiliar = str(auxVar)
+            return auxiliar[2:-3]
+        else:
+            return dest
 
 def zettel_edit(dest: Path, view=False):
+    # If the -v is present open the file and ignore everything else
     if view:
-        # If -v flag present do:
         subprocess.Popen(['okular', dest])
         exit(0)
-    try: # Open or create the note and path
+    # Try open or create the path to the note before creating the note itself.
+    try:
         if not os.path.exists(dest):
             os.makedirs(os.path.split(dest)[0], exist_ok=True)
     except NoteError as f:
         print('Error: Could not open the note', f, file=stderr)
         exit(1)
     finally:
-        with open('utils/lastOpenedNote', 'w') as writeLastNote:
-            writeLastNote.write(str(dest))
-            writeLastNote.close()
+        with open('utils/lastOpenedNote', 'w') as lastNote:
+            lastNote.write(str(dest)) # Rewrites to the lastNote
+        # If the note doesn't exists create it and put template on it
+        if not exists(dest):
+            with open(dest, 'w') as destNote:
+                destNote.write('# Title\n## @\n\n\n')
+        # And open with the default EDITOR
         subprocess.run([os.environ['EDITOR'], dest])
 
-def cli() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(prog='zettelpy',
-            description='Personal Knowledge System based on Zettelkasten')
-    parser.add_argument('destination',
-            type=Path,
-            help='Provide path/Title or Luhmann-ID to go to note')
-    parser.add_argument('-v', '--view',
-            action='store_true',
-            help='Open a note by title, ID, or by lastNoteUsed')
-    return parser.parse_args()
-
 def main():
+    # The arguments come from argparse
     args = cli()
+    # Define default place
     notes_directory = (os.environ['HOME'] + '/.zettelpy')
-
-    slip_box(notes_directory) # Checks environment
-
-    zettel_edit(what_is_this(args.destination), args.view)
+    # Checks environment
+    slip_box(notes_directory)
+    # Edit the dest and note
+    zettel_edit(define_path(args.destination), args.view)
 
 if __name__ == '__main__':
     main()
